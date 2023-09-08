c79316a7f965c753d3588384d6892375731d602a
Abhishek Raj <me_abhishek.tethered@icloud.com>
2023-09-09 01:40:56 +0530

    Added feature that users can see number of online users

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ io.on("connection", (socket) => {
   socket.on("new-user-joined", (userName) => {
     console.log(`${userName} joined the chat`);
     users[socket.id] = userName;
-    socket.broadcast.emit("user-joined", userName);
+    let totalUser = Object.keys(users).length;
+    socket.broadcast.emit("user-joined", userName, totalUser);
+    socket.emit("total-user-online", totalUser);
   });
 
   socket.on("send-message", (msgText) => {
@@ -18,7 +20,9 @@ io.on("connection", (socket) => {
 
   socket.on("disconnect", (message) => {
     console.log(`${users[socket.id]} left the chat`);
-    socket.broadcast.emit("left-chat", users[socket.id]);
+    let totalUser = parseInt(Object.keys(users).length) - 1;
+    socket.broadcast.emit("left-chat", users[socket.id], totalUser);
+    socket.emit("total-user-online", totalUser);
     delete users[socket.id];
   });
 });
